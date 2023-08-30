3893d6de2f5b94f983d99759f18719efbd6879e9
Alexey Kostarev <kaf@altlinux.org>
2023-08-31 00:43:01 +0500

    Изменение правил iptables -s $U7S_SERVICECIDR -d $U7S_SERVICECIDR -j SNAT --to $U7S_EXTIP, добавление IP $U7S_SLIRP4IP на tap0

--- usernetes/bin/u7sinit.sh
+++ usernetes/bin/u7sinit.sh
@@ -3,10 +3,11 @@ source podsec-u7s-functions
 source $envFile
 
 ip a add  $U7S_TAPIP/$U7S_SERVICEMASK dev $U7S_EXTDEV  || :;
-iptables -I POSTROUTING -t nat -d $U7S_SERVICECIDR -j SNAT --to $U7S_TAPIP  || :;
+iptables -I POSTROUTING -t nat -s $U7S_SERVICECIDR -d $U7S_SERVICECIDR -j SNAT --to $U7S_EXTIP  || :;
 if [ U7S_CONTROLPLANE = 'initMaster' ]
 then
   ip a add $U7S_KUBERNETESCLUSTERDNS/$U7S_SERVICEMASK dev $U7S_EXTDEV  || :;
+  ip a add $U7S_SLIRP4IP/$U7S_SERVICEMASK dev $U7S_EXTDEV  || :;
   iptables -t nat -I PREROUTING -p tcp -d 0.0.0.0/0 --dport 53 -j DNAT --to=$U7S_TAPIP:6053  || :;
   iptables -t nat -I PREROUTING -p udp -d 0.0.0.0/0 --dport 53 -j DNAT --to=$U7S_TAPIP:6053  || :;
 fi
